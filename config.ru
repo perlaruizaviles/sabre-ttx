d2f704902b41d0f096313bd34bfabb4e4767709e
dvarela <dvarela@skytouchtechnology.com>
2017-06-24 20:10:06 -0700

    room codes are working now

--- config.ru
+++ config.ru
@@ -1,2 +1,3 @@
 require './lib/app'
 run Sinatra::Application
+
---
+++ lib/room_codes.rb
@@ -0,0 +1,17 @@
+require 'yaml'
+
+module TTx
+    class RoomCodes 
+        attr_reader :room_code_map
+
+        def initialize
+            @room_code_map = YAML.load_file(File.dirname(__FILE__) + '/../assets/room-codes.yml')    
+        end 
+
+        def room_code_to_meaning(code)
+            result = code.downcase.split('').each_with_index.map { |e, i| @room_code_map[i][e] }
+            result.delete_if { |e| e.nil? || e.empty? }.join(', ')
+        end 
+
+    end 
+end
