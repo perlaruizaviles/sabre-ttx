9daab6f2b568e735633937f086bd05286e590914
dvarela <dvarela@skytouchtechnology.com>
2017-06-24 22:19:27 -0700

    headers done

--- lib/ttx/header_builder.rb
+++ lib/ttx/header_builder.rb
@@ -1,7 +1,11 @@
 require 'nokogiri'
 
+require_relative 'assets'
+
 module TTx
     class HeaderBuilder
+        EB_NAMESPACE   = 'http://www.ebxml.org/namespaces/messageHeader'
+        WSSE_NAMESPACE = 'http://schemas.xmlsoap.org/ws/2002/12/secext'
         attr_reader :action, :security_token
 
         def initialize(action, security_token)
@@ -10,8 +14,13 @@ module TTx
         end
 
         def build_header 
-            File.read()
-            Nokogiri::HTML::DocumentFragment.parse()
+            file = TTx::Assets.load_file('header.xml')
+            doc  = Nokogiri::XML::DocumentFragment.parse(file)
+            # puts doc
+            doc.at_xpath('*//eb:Action', eb: EB_NAMESPACE).child.content                 = @action
+            doc.xpath('*//wsse:BinarySecurityToken', wsse: WSSE_NAMESPACE).first.content = @security_token 
+
+            doc
         end
     end 
     
